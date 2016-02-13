6076ac331c0dfbf842bec576cff9dbf581df1359
David Hardiman <david_hardiman@yahoo.co.uk>
2016-02-13 15:46:17 +0000

    Initial commit

---
+++ Fetch/Fetch.h
@@ -0,0 +1,19 @@
+//
+//  Fetch.h
+//  Fetch
+//
+//  Created by David Hardiman on 13/02/2016.
+//  Copyright © 2016 David Hardiman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Fetch.
+FOUNDATION_EXPORT double FetchVersionNumber;
+
+//! Project version string for Fetch.
+FOUNDATION_EXPORT const unsigned char FetchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fetch/PublicHeader.h>
+
+
---
+++ FetchTests/FetchTests.swift
@@ -0,0 +1,36 @@
+//
+//  FetchTests.swift
+//  FetchTests
+//
+//  Created by David Hardiman on 13/02/2016.
+//  Copyright © 2016 David Hardiman. All rights reserved.
+//
+
+import XCTest
+@testable import Fetch
+
+class FetchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

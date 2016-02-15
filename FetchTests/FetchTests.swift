68356660e76c1a8960a9a099caafe4c21bada833
Dave Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2016-02-15 15:33:05 +0000

    Add test response object

--- FetchTests/FetchTests.swift
+++ FetchTests/FetchTests.swift
@@ -9,28 +9,33 @@
 import XCTest
 @testable import Fetch
 
+let testString = "{ \"name\": \"test name\", \"desc\": \"test desc\" }"
+
+struct TestResponse: Parsable {
+    enum Fail: ErrorType {
+        case StatusFail
+        case ParseFail
+    }
+
+    let name: String
+    let desc: String
+
+    static func parse(fromData data: NSData, withStatus status: Int) -> Result<TestResponse> {
+        if status != 200 {
+            return .Failure(Fail.StatusFail)
+        }
+        do {
+            if let dict = try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [String: String] {
+                return .Success(TestResponse(name: dict["name"]!, desc: dict["desc"]!))
+            }
+        } catch {}
+        return .Failure(Fail.ParseFail)
+
+    }
+}
+
 class FetchTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
+
     
 }

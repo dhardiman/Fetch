289f4d91a95cfbc1294a6bdf64ed39905fd65443
Dave Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2016-02-15 15:58:20 +0000

    Initial happy case tests

--- FetchTests/FetchTests.swift
+++ FetchTests/FetchTests.swift
@@ -7,9 +7,13 @@
 //
 
 import XCTest
+import Nimble
+import MIQTestingFramework
 @testable import Fetch
 
-let testString = "{ \"name\": \"test name\", \"desc\": \"test desc\" }"
+let testString: NSString = "{ \"name\": \"test name\", \"desc\": \"test desc\" }"
+let testURL = NSURL(string: "https://fetch.davidhardiman.me")!
+let basicRequest = Request(url: testURL)
 
 struct TestResponse: Parsable {
     enum Fail: ErrorType {
@@ -35,7 +39,61 @@ struct TestResponse: Parsable {
 }
 
 class FetchTests: XCTestCase {
-    
 
+    override func tearDown() {
+        OHHTTPStubs.removeAllStubs()
+        super.tearDown()
+    }
+
+    func stubRequest(statusCode statusCode: Int32 = 200, passingTest test: OHHTTPStubsTestBlock) {
+        OHHTTPStubs.stubRequestsPassingTest(test) { (request) -> OHHTTPStubsResponse in
+            return OHHTTPStubsResponse(data: testString.dataUsingEncoding(NSUTF8StringEncoding)!, statusCode: 200, headers: nil)
+        }
+    }
     
+    func testItIsPossibleToMakeAGetRequest() {
+        stubRequest { (request) -> Bool in
+            return request.URL! == testURL && request.HTTPMethod == "GET"
+        }
+
+        let expectation = expectationWithDescription("get request")
+        var receivedResult: Result<TestResponse>?
+        Fetch.get(basicRequest) { (result: Result<TestResponse>) in
+            receivedResult = result
+            expectation.fulfill()
+        }
+        waitForExpectationsWithTimeout(1.0, handler: nil)
+        guard let testResult = receivedResult else {
+            fail("Should have received a result")
+            return
+        }
+        switch testResult {
+        case .Success(let response):
+            expect(response.name).to(equal("test name"))
+            expect(response.desc).to(equal("test desc"))
+        default:
+            fail("Should be a successful response")
+        }
+    }
+
+    func testItIsPossibleToMakeAPostRequest() {
+        stubRequest { (request) -> Bool in
+            return request.URL! == testURL && request.HTTPMethod == "POST"
+        }
+        let expectation = expectationWithDescription("post request")
+        var receivedResult: Result<TestResponse>?
+        Fetch.post(basicRequest) { (result: Result<TestResponse>) in
+            receivedResult = result
+            expectation.fulfill()
+        }
+        waitForExpectationsWithTimeout(1.0, handler: nil)
+        switch receivedResult! {
+        case .Success(_):
+            break
+        default:
+            fail("Should be a successful response")
+        }
+    }
+
+
 }

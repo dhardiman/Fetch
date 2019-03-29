25cdc8015649a2b012e47b124e38b2b76ad0cdde
David Hardiman <david_hardiman@yahoo.co.uk>
2019-03-29 14:10:26 +0000

    Update package to swift 5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,5 +12,6 @@ let package = Package(
     targets: [
       .target(name: "Fetch", dependencies: []),
       .testTarget(name: "FetchTests", dependencies: ["Fetch"])
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
